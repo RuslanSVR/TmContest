e6667c014477d61aa21f5fe3b48beeb323df4ccf
RuslanSVR <ruslanpost@inbox.ru>
2019-04-13 17:09:22 +0300

    f_getDate release 2.0

--- main.js
+++ main.js
@@ -1,18 +1,26 @@
 var uTime = 1528016400000;
     ch_max = 5;
-function f_getDate(UNIX_timestamp, tView){ //tView[t - time, d - date]
+function f_getDate(UNIX_timestamp, tView){ //tView[t - time, d1 - 1 Apr, d2 - 1 April 2019, day - Saturday]
   var uStamp = new Date(UNIX_timestamp);
-  var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+  var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+  var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
   var time = 'error: Invalid view type';
-  if (tView == 'd') {
-      time = uStamp.getDate() + ' ' + months[uStamp.getMonth()];
+  if (tView == 'd1') {
+      time = uStamp.getDate() + ' ' + (months[uStamp.getMonth()]).substr(3);
+  } else if (tView == 'd2') {
+      time = uStamp.getDate() + ' ' + months[uStamp.getMonth()] + ' ' + uStamp.getFullYear();
   } else if (tView == 't') {
       time = ('0' + uStamp.getHours()).substr(-2) + ':' + ('0' + uStamp.getMinutes()).substr(-2);
+  } else if (tView == 'day') {
+      time = days[uStamp.getDate()];
   }
   return time;
 }
 
-console.log('Time: ' + f_getDate(uTime,'t'));
+console.log('Time_t: ' + f_getDate(uTime,'t'));
+console.log('Time_d1: ' + f_getDate(uTime,'d1'));
+console.log('Time_d2: ' + f_getDate(uTime,'d2'));
+console.log('Time_day: ' + f_getDate(uTime,'day'));
 
 
 function getChart(url){
@@ -39,7 +47,7 @@ for (var i = 1; i <= ch_max; i++) {
   );
 }
 
-for (var i = 0; i < ch_max; i++) {
+for (var i = 1; i <= ch_max; i++) {
   document.write("<div><font class='chartTitles' id='chartTitle"+ i + "'>" + "Chart: #" + (i+1) + "</font></div>"); //create title element for each chart
 
 }
