c4618482f5e01fa15dd271ab8b57ac91e49fd175
RuslanSVR <ruslanpost@inbox.ru>
2019-04-14 20:44:34 +0300

    1

--- main.js
+++ main.js
@@ -87,18 +87,8 @@ function onSuccess(charts) {
       for (let j = 1; j < charts[1].columns[i].length; j++)
         charts[1].columns[i][j] = Math.round(charts[1].columns[i][j]*scaling_k);
       //end of scaling
-      /*
-      document.write("<polyline points=\"" + '10,20 100,300' + "\" stroke=" + "red");
-      document.write(" stroke-width=\"1\" fill=\"transparent\" vector-effect=\"non-scaling-stroke\"  ");
-      document.write(" />");
-      */
 
-      var points_el = document.createElement("polyline");
-      points_el.setAttribute('points', '20,20 40,25 60,40 80,120 120,140 200,180');
-      points_el.setAttribute('stroke', "red");
-      points_el.setAttribute('stroke-width', '3');
-      points_el.setAttribute('fill', 'black');
-      document.getElementById('chartBoard' + charts[0]).appendChild(points_el);
+      document.getElementById('chartBoard' + charts[0]).innerHTML = "<polyline points='20,20 40,50 60,40' stroke='red' stroke-width='3' fill='none' vector-effect='non-scaling-stroke' />";
 
       console.info('Chart# ',charts[0], ' y_scaled',charts);
   } else if ((charts[1].types.y0) == "bar") {
@@ -114,7 +104,7 @@ function onSuccess(charts) {
 let x1 = 0; let y1 = 0; //delete from here
 for (let i = 1; i <= 5 ; i++) {
   document.write("<div class='chartsHeader'><div class='chartsName'><font class='chartTitles' id='chartTitle"+ i + "'>" + "Chart: #" + i + "</font></div><div class='chart-interval'><font class=date-interval id=date-interval-" + i + ">Test</font></div></div>"); //create title element for each chart
-  document.write("<div><svg x='0' y='0' width='400px' height=400px viewbox='0 0 400 400' preserveAspectRatio='none' class='chartBoards' id='chartBoard"+ i + "' xmlns=\"http://www.w3.org/2000/svg\" " + "" + ">"); //create chart board i
+  document.write("<div><svg x='0' y='0' width='400px' height=400px  preserveAspectRatio='none' class='chartBoards' id='chartBoard"+ i + "' xmlns=\"http://www.w3.org/2000/svg\" " + "" + ">"); //create chart board i
   document.write("</div></svg>");
 }
 
